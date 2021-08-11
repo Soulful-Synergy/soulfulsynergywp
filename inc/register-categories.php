29923acdbe8e4e0b7ee15d8169c2a848da7c4898
Eric Benedetto <ericsplace3@gmail.com>
2021-08-11 13:45:40 -0400

    fix taxonomy issues

--- inc/register-categories.php
+++ inc/register-categories.php
@@ -2,15 +2,15 @@
 
 function soulfulsynergy_register_categories() {
     // Pathways
-    wp_insert_term('Individual','pathways');
-    wp_insert_term('Business','pathways');
-    wp_insert_term('Government','pathways');
-    wp_insert_term('Non-Profit','pathways');
+    if(!term_exists('individual')) wp_insert_term('Individual','pathways');
+    if(!term_exists('business')) wp_insert_term('Business','pathways');
+    if(!term_exists('government')) wp_insert_term('Government','pathways');
+    if(!term_exists('non-profit')) wp_insert_term('Non-Profit','pathways');
 
     // Course Categories
-    wp_insert_term('In-Person','course_cats');
-    wp_insert_term('Online','course_cats');
-    wp_insert_term('Hybrid','course_cats');
+    if(!term_exists('in-person')) wp_insert_term('In-Person','course_cats');
+    if(!term_exists('online')) wp_insert_term('Online','course_cats');
+    if(!term_exists('hybrid')) wp_insert_term('Hybrid','course_cats');
 
 }
 
